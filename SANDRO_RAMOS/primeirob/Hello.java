4892fbbe095f07dea072c863980aa58bf0947c00
cmagagnin <cmagagnin@minha.fag.edu.br>
2024-08-06 20:12:11 -0300

    teste

--- SANDRO_RAMOS/primeirob/Hello.java
+++ SANDRO_RAMOS/primeirob/Hello.java
@@ -2,6 +2,6 @@ package SANDRO_RAMOS.primeirob;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+        System.out.println("Olá mund!");
     }
 }
