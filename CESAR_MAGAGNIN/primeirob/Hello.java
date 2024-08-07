10a151f9f6e40fece02249aff8fc160ec562694b
cmagagnin <cmagagnin@minha.fag.edu.br>
2024-08-06 21:50:51 -0300

    RETIFICAÇÃO DA PASTA

--- CESAR_MAGAGNIN/primeirob/Hello.java
+++ CESAR_MAGAGNIN/primeirob/Hello.java
@@ -2,6 +2,6 @@ package CESAR_MAGAGNIN.primeirob;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Olá mundo - tester!");
+        System.out.println("Olá world - tester!");
     }
 }
