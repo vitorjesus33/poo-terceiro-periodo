f7c8dabe6b1203f6647bd6e84ddaaf0fd7732d3c
Jacy Cordeiro <jcordeiro1@minha.fag.edu.br>
2024-08-13 19:18:19 -0300

    Calculadora pronta

---
+++ JACY_CORDEIRO/primeirob/aulas/CALCULADORA_JAVA/CALCULADORA/src/poo/Calculadora.java
@@ -0,0 +1,55 @@
+package poo;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Operacoes n = new Operacoes(); // Verifique se essa linha está correta
+
+        System.out.println("Escolha uma operação: ");
+        System.out.println("1. Soma");
+        System.out.println("2. Subtração");
+        System.out.println("3. Multiplicação");
+        System.out.println("4. Divisão");
+        
+        int operacao = scan.nextInt();
+
+        System.out.println("Digite o primeiro número: ");
+        double x = scan.nextDouble();
+
+        System.out.println("Digite o segundo número: ");
+        double y = scan.nextDouble();
+
+        switch (operacao) {
+            case 1:
+                imprimeCalculadora(operacao, n.soma(x, y), x, y);
+                break;
+            case 2:
+                imprimeCalculadora(operacao, n.subtracao(x, y), x, y);
+                break;
+            case 3:
+                imprimeCalculadora(operacao, n.multiplicacao(x, y), x, y);
+                break;
+            case 4:
+                imprimeCalculadora(operacao, n.divisao(x, y), x, y);
+                break;
+            default:
+                System.out.println("Operação inválida!");
+        }
+
+        scan.close();
+    }
+
+    static void imprimeCalculadora(int operacao, double resultado, double x, double y) {
+        HashMap<Integer, String> mapOperacao = new HashMap<>();
+        mapOperacao.put(1, "somado");
+        mapOperacao.put(2, "subtraído");
+        mapOperacao.put(3, "multiplicado");
+        mapOperacao.put(4, "dividido");
+
+        System.out.println("\nO resultado de " + x + " " + mapOperacao.get(operacao) + " por " + y + " é igual a " + resultado + "\n");
+    }
+}
