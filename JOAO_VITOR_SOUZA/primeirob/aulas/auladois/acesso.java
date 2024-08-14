b65c7f0355b0e717f092c80ed851242e8fb2bce5
jvssilva2 <jvssilva2@minha.fag.edu.br>
2024-08-14 19:34:00 -0300

    Entrega lista um

---
+++ JOAO_VITOR_SOUZA/primeirob/aulas/auladois/acesso.java
@@ -0,0 +1,10 @@
+package JOAO_VITOR_SOUZA.primeirob.aulas.auladois;
+
+public class acesso {
+
+    protected int idadeDois;
+    protected static final int valorImutavel = 0;
+    public static int getValorimutavel() {
+        return valorImutavel;
+    }
+}
