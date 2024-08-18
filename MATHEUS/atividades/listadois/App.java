dc504d2996d9b637e4c693b2ee1a7ff8f1282040
Matheus <matheus22lima2005@gmail.com>
2024-08-18 20:46:39 -0300

    Lista 2 concluida

--- MATHEUS/atividades/listadois/App.java
+++ MATHEUS/atividades/listadois/App.java
@@ -1,42 +1,33 @@
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.List;
 
 public class App {
+    
+    
     public static void main(String[] args) throws Exception {
-        float valorUnitarioPlanta, dinheiroRecebido, valorTotal = 0;
-        int quantidadePlanta;
+        
+        List<Produto> listaDeProdutos = new LinkedList<>();
         int menu;
-        try (Scanner entrada = new Scanner(System.in)) {
-            do {
-                System.out.println("selecione a opção doadfasdfasdf menu:\n1 - registar planta\n2 - finaliza venda\n0 - sair do sistema");
-                menu = entrada.nextInt();
 
-                switch (menu) {
-                    case 1 -> {
-                        // grava venda
-                        System.out.println("digite o valor da planta comprada...");
-                        valorUnitarioPlanta = entrada.nextFloat();
-                        System.out.println("quantidade de plantas compradas");
-                        quantidadePlanta = entrada.nextInt();
-                        valorTotal = quantidadePlanta * valorUnitarioPlanta;
-                    }
-                    case 2 -> {
-                        // finaliza venda
-                        System.out.println("Digite o valor, pago valor total da compra = " + valorTotal);
-                        dinheiroRecebido = entrada.nextFloat();
-                        if (dinheiroRecebido < valorTotal) {
-                            System.out.println("Pagamento insuficiente, valor faltante " + (valorTotal - dinheiroRecebido));
-                            break;
-                        }
-                        System.out.println("seu troco é: " + (dinheiroRecebido - valorTotal) + "\n compra concluida");
-                    }
-                    case 0 -> {
-                        System.out.println("sistema finalizado aperte entre para sair...");
-                        entrada.nextLine();
-                    }
-                    default -> {
-                    }
+        do {
+            menu = Menu.exibeMenu();
+
+            switch (menu) {
+                case 1 -> {
+                    Menu.gravaVenda(listaDeProdutos);
                 }
-            } while (menu != 0);
-        }
+                case 2 -> {
+                    Menu.finalizaVenda(listaDeProdutos);
+                }
+                case 3 ->{
+                    Menu.exibeHistorico();
+                }
+                case 0 -> {                    
+                    Menu.finalizaSistema();
+                }
+                default -> {
+                }
+            }
+        } while (menu != 0);
     }
 }
