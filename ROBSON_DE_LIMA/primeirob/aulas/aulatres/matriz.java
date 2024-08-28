771314fbedaf0fe16b7ae7d507b9491b7c478557
Robson <rblima@minha.fag.edu.br>
2024-08-28 13:25:57 -0300

    listas 3 e 4

---
+++ ROBSON_DE_LIMA/primeirob/aulas/aulatres/matriz.java
@@ -0,0 +1,11 @@
+package ROBSON_DE_LIMA.primeirob.aulas.aulatres;
+
+public class matriz {
+    public static void main(String[] args) {
+        int[][] matriz1 = {{31,27,4},{44,32,95},{9,16,56}};
+        int[][] matriz2 = {{10,88,12},{38,32,73},{14,21,52}};
+
+        
+    }
+
+}
--- ROBSON_DE_LIMA/primeirob/listas/listatres/main.java
+++
@@ -1,5 +0,0 @@
-package ROBSON_DE_LIMA.primeirob.listas.listatres;
-
-public class main {
-    
-}
