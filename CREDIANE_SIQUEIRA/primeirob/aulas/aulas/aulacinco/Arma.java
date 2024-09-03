794fefe5bc21942dc6f6c3d713cc01b75c9534ba
Crediane <csiqueira@minha.fag.edu.br>
2024-09-03 20:50:12 -0300

    Lista quatro markdown

---
+++ CREDIANE_SIQUEIRA/primeirob/aulas/aulas/aulacinco/Arma.java
@@ -0,0 +1,15 @@
+package CREDIANE_SIQUEIRA.primeirob.aulas.aulas.aulacinco;
+
+public class Arma {
+    String nome;
+    String skin;
+    String tipo;
+    
+    Integer dano;
+    
+    
+
+
+
+
+}
