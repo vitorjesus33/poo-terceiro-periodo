6b4f397568725b08eefc07a4272cda03f41e620e
mits0014 <matheus22lima2005@gmail.com>
2024-09-04 21:45:38 -0300

    lista 5

---
+++ MATHEUS/atividades/listacinco/Vendedor.java
@@ -0,0 +1,9 @@
+package MATHEUS.atividades.listacinco;
+
+public class Vendedor {
+    public String nome;
+    public int java;
+    public double[] salrioRecebido;
+    public Loja loja;
+    public String salarioBase;
+}
